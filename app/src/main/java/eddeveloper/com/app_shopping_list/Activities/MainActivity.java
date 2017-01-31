5c744b68d7a4a37140382f27f11ee92a05b4c6c2
EAdeveloper <eddeveloper@hotmail.com>
2017-01-31 15:42:07 -0500

    VId18- Create ans install Sql database "dbList", instal dependencies  in gradle for design and recyclerview

--- app/src/main/java/eddeveloper/com/app_shopping_list/Activities/MainActivity.java
+++ app/src/main/java/eddeveloper/com/app_shopping_list/Activities/MainActivity.java
@@ -3,11 +3,9 @@ package eddeveloper.com.app_shopping_list.Activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import eddeveloper.com.app_shopping_list.EnviarMensaje;
-import eddeveloper.com.app_shopping_list.Fragments.rigth;
 import eddeveloper.com.app_shopping_list.R;
 
-public class MainActivity extends AppCompatActivity implements EnviarMensaje {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,9 +13,5 @@ public class MainActivity extends AppCompatActivity implements EnviarMensaje {
         setContentView(R.layout.activity_main);
     }
 
-    @Override
-    public void enviarDatos(String mensaje) {
-        rigth derecha = (rigth)getFragmentManager().findFragmentById(R.id.derecha);
-        derecha.ObtenerDatos(mensaje);
-    }
+
 }
